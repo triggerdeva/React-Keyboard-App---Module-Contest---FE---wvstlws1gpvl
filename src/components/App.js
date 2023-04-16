5e5620ce3dab18dc0591b1de47aa175cefd2e6a0
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-16 10:11:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,44 @@
 import "../styles/App.css";
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
+  const [preview, setPreview] = useState('');
+  const [quote, setQuote] = useState('');
+
+  const handleClick = (keyValue) => {
+    const updatedPreview = preview + keyValue;
+    setPreview(updatedPreview);
+
+    if (updatedPreview === 'forty two') {
+      axios.get('https://api.quotable.io/random').then((response) => {
+        setQuote(response.data.content);
+      });
+    } else {
+      setQuote('');
+    }
+  };
+  const renderKeys = () => {
+   
+    return keys.map((keyValue) => (
+      <button key={`key-${keyValue}`} id={`key-${keyValue === ' ' ? 'space' : keyValue}`} onClick={() => handleClick(keyValue)}>
+        {keyValue}
+      </button>
+    ));
+  };
 
   return (
-    <div className="keyboard">
-      <div className="preview"></div>
-      <div>
-        {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
-            {key === " " ? "Space" : key.toUpperCase()}
-          </button>
-        ))}
-      </div>
+    <div className="App">
+      <div className="preview">{preview}</div>
+      {preview === 'forty two' && <div className="quote">{quote}</div>}
+      <div className="keyboard">{renderKeys()}</div>
     </div>
   );
+
 };
+
+
 
 export default App;
